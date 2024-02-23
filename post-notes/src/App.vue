5f686ac05bedf45a24d6110a7b1a2861468bb0f6
MariaTheresaAzzi <mariatheresaelkazzi@gmail.com>
2024-02-23 11:40:00 +0200

    done

--- post-notes/src/App.vue
+++ post-notes/src/App.vue
@@ -1,14 +1,54 @@
+<script setup>
+import { ref } from "vue";
+
+const showModal = ref(false);
+const newNote = ref("");
+const errorMessage = ref("");
+const notes = ref([]);
+
+function getRandomColor() {
+  return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+}
+
+const addNote = () => {
+  if(newNote.value.length < 10){
+    return errorMessage.value = "Note needs to be 10 characters or more"
+  }
+  notes.value.push({
+    id: Math.floor(Math.random() * 1000000),
+    text: newNote.value,
+    date: new Date(),
+    backgroundColor: getRandomColor()
+    });
+    showModal.value = false;
+    newNote.value = "";
+    errorMessage.value = "";
+}
+</script>
+
 <template>
   <main>
-    <div>
+    <div class="overlay" v-if="showModal">
+      <div class="modal">
+        <textarea v-model.trim="newNote" name="note" id="note" cols="30" rows="10"></textarea>
+        <p class="error" v-if="errorMessage">{{ errorMessage }}</p>
+        <button class="add-Note"  @click="addNote()">Add Note</button>
+        <button class="close" @click="showModal = false">Close</button>
+      </div>
+    </div>
+    <div class="container">
       <header>
-      <h1>Notess</h1>
-        <button>+</button>
+        <h1>Notes</h1>
+        <button class="add" @click="showModal = true">+</button>
       </header>
-      <div>
-        <div>
-          <p></p>
-          <p></p>
+      <div class="cards-container">
+        <div class="card" v-for="note in notes"
+        :key="note.id"
+        :style="{backgroundColor: note.backgroundColor}">
+          <p class="main-text">
+            {{ note.text }}
+          </p>
+          <p class="date">{{ note.date.toLocaleDateString("en-US") }}</p>
         </div>
       </div>
     </div>
